f0132c3be174bd84d6a67bbc96a3c74af7d91b15
Georgi Georgiev <georgighomework@gmail.com>
2024-03-04 10:49:57 +0200

    Create kids collection in group

--- Kindergarden_Models/Groups.cs
+++ Kindergarden_Models/Groups.cs
@@ -8,9 +8,14 @@ namespace Kindergarden_Models
 {
 	public class Groups
 	{
-		public int GroupId { get; set; }
+        public Groups()
+        {
+                this.KidInGroup = new Hashset<Kids>();
+        }
+        public int GroupId { get; set; }
 
         public string GroupName { get; set; }
 
+		public ICollection<Kids> KidInGroup { get; set; }	w
     }
 }
