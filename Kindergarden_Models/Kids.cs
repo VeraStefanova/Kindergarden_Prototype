1acf9429dc6290af8bcf8b701eff932b25dc152f
Kamen Komsiyski <kammen06@gmail.com>
2024-03-04 13:16:18 +0200

    Fix ForeignKey Relations

--- Kindergarden_Models/Kids.cs
+++ Kindergarden_Models/Kids.cs
@@ -13,7 +13,9 @@ namespace Kindergarden_Models
         public string LastName { get; set; }
         public int Age { get; set; }
         public int ParentId { get; set; }
+        public virtual Parents Parent { get; set; }
         public int GroupId { get; set; }
+        public virtual Groups Group { get; set; }
 
 
 
