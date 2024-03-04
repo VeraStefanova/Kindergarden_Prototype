b534d006581b2b08b6bae945da4e85d4d2bb2e05
Kamen Komsiyski <kammen06@gmail.com>
2024-03-04 12:44:27 +0200

    Fname and Lname of kids

--- Kindergarden_Models/Kids.cs
+++ Kindergarden_Models/Kids.cs
@@ -1,9 +1,15 @@
-﻿namespace Kindergarden_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kindergarden_Models
 {
     public class Kids
     {
         public int KidId { get; set; }
+        
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
         public int Age { get; set; }
         public int ParentId { get; set; }
