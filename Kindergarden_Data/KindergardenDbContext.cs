7aacd185a221c1c2b8d271bb518188a19ac39c17
Kamen Komsiyski <kammen06@gmail.com>
2024-03-17 11:52:20 +0200

    kkk

--- Kindergarden_Data/KindergardenDbContext.cs
+++ Kindergarden_Data/KindergardenDbContext.cs
@@ -28,7 +28,7 @@ namespace Kindergarden_Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=LEGIONPRO5\\SQLEXPRESS;Database=Kindergarden;Integrated Security=true;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=KAMENPC\\SQLEXPRESS;Database=Kindergarden;Integrated Security=true;TrustServerCertificate=True");
             }
 
         }
