8a4074496d3b93ceaa69cf5e072b90f17b9f3f55
Kamen Komsiyski <kammen06@gmail.com>
2024-03-24 02:04:16 +0200

    Second Commit

--- Kindergarden_Models/Parent.cs
+++ Kindergarden_Models/Parent.cs
@@ -17,7 +17,7 @@ namespace Kindergarden_Models
         /// <summary>Gets or sets the first name.</summary>
         /// <value>The first name.</value>
         public string FirstName { get; set; }
-        /// <summary>Gets or sets the last name.</summary>
+        /// <summary>Gets or sets the last name.</summary> 
         /// <value>The last name.</value>
         public string LastName { get; set; }
         /// <summary>Gets or sets the phone number.</summary>
